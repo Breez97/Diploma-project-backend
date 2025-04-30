153a13628a551cdcf4bc64311a27a06dbd59f48e
Ilya Shamrov <ilya66401@gmail.com>
2025-04-30 21:35:09 +0300

    upd: beginning of service-users

--- service-users/src/main/java/com/breez/exception/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.breez.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-
-	public UserAlreadyExistsException(String message) {
-		super(message);
-	}
-
-}
